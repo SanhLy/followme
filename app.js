c6181afc2807c19dbb7dc46d5c13d9b4bcbef9f4
SanhLY <sanh.ly1@gmail.com>
2016-01-17 17:07:42 +0100

    Cleaning comments, adding some extra messages in the chat and cleaning the code

--- app.js
+++ app.js
@@ -14,19 +14,35 @@ app.use(express.static('public'));
 var baseAddress = "http://192.168.1.52:8080"
 
 app.get('/', function (req, res) {
-  res.render('connect.ejs', {});
+  res.render('connect.ejs', {room:req.session.room});
 })
 .post('/followme/create/',urlencodedParser, function (req, res) {
+    ///////////////////
+    //Inscription Form 
+    //////////////////
+
     req.session.username = req.body.username;
-    req.session.fomeRoom = req.body.followuser || req.session.username;
-    console.log("- User "+req.session.username + " created.");
+    req.session.room = req.body.room || req.session.username;
+    console.log("User "+req.session.username + " created.");
     //If the room is not specified, we create his own room
-    res.redirect('/followme/user/'+req.session.fomeRoom);
+    res.redirect('/followme/user/'+req.session.room);
 
 })
-.get('/followme/user/:user', function (req, res) {
-  console.log("- User "+req.session.username + " : accessing room.");
-  res.render('room.ejs', {username: req.session.username, baseAddress:baseAddress, room:req.session.fomeRoom});
+.get('/followme/user/:room', function (req, res) {
+    ////////////////////
+    //ROOM
+    ////////////////////
+    console.log("Connecting to : "+req.params.room);
+    if (req.session.username == null) {
+        //Someone tries to connect to an existing room without being identified.
+        console.log("User unindentified");
+        req.session.room = req.params.room;
+        res.redirect('/');
+    } else {
+        //Everything's fine, accessing existing room or creating one.
+        // console.log("User "+req.session.username + " accessing room "+req.session.room);
+        res.render('room.ejs', {username: req.session.username, baseAddress:baseAddress, room:req.session.room});    
+    }
 })
 .use(function(req, res, next){
     res.setHeader('Content-Type', 'text/plain');
@@ -40,34 +56,17 @@ app.get('/', function (req, res) {
 
 // // Chargement de socket.io
 var io = require('socket.io').listen(app.listen(8080));
-var userNo = 1;
-var masterId = '';
-var buzzOrder = 0;
-var teamList = [];
-
+var util = require('util');
 
 // Quand on client se connecte, on le note dans la console
 io.sockets.on('connection', function (socket) {
-    
-    function initOrder() {
-        buzzOrder = 0;
-        for (var i=0; i<teamList.length; i++){
-            teamList[i].buzzOrder = 0;
-        }
-    }
 
-    // Actions MASTER
-
-    //Quand le serveur reçoit un message du master   
-    // socket.on('message', function (message) {
-    //     console.log('master : ' + message);
-    //     socket.broadcast.emit('displayMessage',message);
-    // });
-
-    // Connexion du master
     socket.on('user-connection', function (username, room) {
         console.log(username + ' joins ' + room);
+        socket.user = username;
+        socket.room = room;
         socket.join(room);
+        io.to(room).emit('receive-chat', "admin", username + ' joined');
     });
 
     socket.on('chat-message', function(username, message, room) {
@@ -75,79 +74,43 @@ io.sockets.on('connection', function (socket) {
     })
 
     socket.on('position', function (longitude, latitude, username, room) {
-        console.log(username+' sends position '+room);
+        //console.log(username+' sends position '+room);
         if (room) {
             //follower
             io.to(room).emit('position', longitude, latitude, username);
         } else {
             //leader
-            io.to(socket.fomeRoome).emit('position', longitude, latitude, username);
+            io.to(socket.room).emit('position', longitude, latitude, username);
         }
     });
 
-    //Début du round
-    // socket.on('start', function () {
-    //     console.log('-- Round begins ');
-    //     initOrder();
-    //     io.to(masterId).emit('teamList',teamList);
-    //     socket.broadcast.emit('start');
-    // });
+    socket.on('disconnect', function() {
+      io.to(socket.room).emit('receive-chat', "admin", socket.user + ' has left');
+    });
 
-    // //Fin du round   
-    // socket.on('stop', function () {
-    //     console.log('-- Fin du round ');
-    //     socket.broadcast.emit('stop');
-    // });
 
-    // Actions PARTICIPANTS
-    // Quand un participant buzz
-    // socket.on('buzz', function () {
-    //     buzzOrder++;
-    //     for (var i=0; i<teamList.length; i++){
-    //         if (teamList[i].teamSocketId == socket.id){
-    //             teamList[i].buzzOrder = buzzOrder;
-    //             break;
-    //         }
-    //     }
-    //     console.log(socket.teamName +' a buzzé ' + buzzOrder);
-    //     io.to(masterId).emit('teamList',teamList);
-    //     socket.emit('order', teamList);
-    // });
-
-        // Quand le serveur reçoit le login utilisateur
-    // socket.on('team-connection', function (teamName) {
-    //     if (teamName != null && teamName !=''){
-    //         //On a le nom de l'équipe
-    //         socket.teamName = teamName;
-    //         socket.socketId = socket.id;
-    //     } else {
-    //         socket.teamName = 'team'+userNo;
-    //         userNo++;
-    //     }
-    //     console.log("- Team "+socket.teamName+' connected, socketId :' +socket.id);
-    //     teamList.push({teamName : socket.teamName , teamSocketId : socket.id, buzzOrder:0});
-    //     io.to(masterId).emit('teamList',teamList);
-    // });
-
-     
-    // socket.on('disconnect', function() {
-    //   if (socket.id != masterId) {
-    //     // a team disconnects
-    //     console.log("- Team "+socket.teamName + ' is disconnected');
-    //     teamList = teamList
-    //            .filter(function (el) {
-    //                     return el.teamSocketId !== socket.socketId;
-    //                    });
-    //     io.to(masterId).emit('teamList',teamList);
-    //   } else {
-    //     // the quizz master left 
-    //     console.log("- Quiz Master is disconnected");
-    //     socket.broadcast.emit('stop');
-    //   }
-
-    // });
 
 
 });
+
+//divers
+function findClientsSocket(roomId, namespace) {
+    var res = []
+    , ns = io.of(namespace ||"/");    // the default namespace is "/"
+
+    if (ns) {
+        for (var id in ns.connected) {
+            if(roomId) {
+                var index = ns.connected[id].rooms.indexOf(roomId) ;
+                if(index !== -1) {
+                    res.push(ns.connected[id]);
+                }
+            } else {
+                res.push(ns.connected[id]);
+            }
+        }
+    }
+    return res;
+}
 
 
